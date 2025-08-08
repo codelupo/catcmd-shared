f2203b081072585c5ac2338857b590bc18521058
codelupo <codelupo@gmail.com>
2025-08-08 17:05:07 +0100

    Chat Commands pydantic

--- catcmd_shared/models/live_chat.py
+++ catcmd_shared/models/live_chat.py
@@ -1,7 +1,32 @@
-from pydantic import BaseModel
-from typing import Optional, Literal
+from pydantic import BaseModel, Field
+from typing import Optional, Literal, Annotated, Union
 from datetime import datetime
 
+class ChatMsg(BaseModel):
+    msg_id: str
+    platform: Literal["twitch", "youtube", "discord"]
+    user_id: str 
+    username: str
+    msg: str
+    timestamp:datetime
+
+
+class ChatCmd(BaseModel):
+    id: str
+    platform: Literal["twitch", "youtube", "discord"]
+    user_id: str
+    username: str
+    cmd: str
+    timestamp: datetime
+
+class CmdDiscord(ChatCmd):
+    id: Literal["discord"]
+    args: Optional[str] = None
+
+CmdUnion = Annotated[
+    Union[CmdDiscord, ],
+    Field(discriminator="command"),
+]
 
 
 class LiveChatMsg(BaseModel):
